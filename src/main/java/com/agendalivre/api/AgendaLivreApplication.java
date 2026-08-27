f1b41618e442ca3315b3a92d8be3e3f90f144aed
Pedro Aurélio <pedroaurelio.reis@gmail.com>
2026-08-27 11:37:19 -0300

    feat(setup): inicializa projeto Spring Boot com estrutura Clean Architecture

---
+++ src/main/java/com/agendalivre/api/AgendaLivreApplication.java
@@ -0,0 +1,13 @@
+package com.agendalivre.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaLivreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaLivreApplication.class, args);
+    }
+
+}
